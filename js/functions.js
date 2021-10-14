7c2e833bcd0a464c57edd51923989123ce67821c
Carlos <darksunreaper@gmail.com>
2021-10-14 20:15:44 +0200

    Punto 9

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,11 @@
 var flipped = 0;
 
 function flip(card){
+	if (flipped >= 2){return}
 	if (card.getAttribute('state')=='unfliped'){
 		card.firstElementChild.setAttribute('src','../images/cruz.jpg');
 		card.setAttribute('state', 'fliped');
-		//+1 a flipped
+		flipped++;
 	}
 	//if 2 flipped, comparar
 	//si son iguales se dejan arriba
