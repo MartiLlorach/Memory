fe95312f7a0cd5e0f88e19471c061fc7c956cc1a
martiLlo <martillorachnaharro@gmail.com>
2021-10-15 16:17:06 +0200

    first version of the winner page

---
+++ winner.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>memory</title>
+	<link rel="stylesheet" type="text/css" href="./styles/winner.css">
+</head>
+<body>
+	<div class="main">
+		<div class="winner">
+			<h1>YOU WIN!</h1>
+		</div>
+		<div class="bottom-row">
+			<div class="bottom-left">
+				<button id="playAgain" class="btn btn-primary">Play again</button>
+			</div>
+			<div class="bottom-right">
+				<button id="mainPage" class="btn btn-primary">Turn to main page</button>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
