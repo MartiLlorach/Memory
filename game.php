7751d62234594d73fc0222a880abbaf8fcf19d75
Carlos <darksunreaper@gmail.com>
2021-10-20 18:06:21 +0200

    changed gameStyle.css to game.css

--- game.php
+++ game.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>Memory - Game</title>
-	<link rel="stylesheet" href="./styles/gameStyle.css" type="text/css">
+	<link rel="stylesheet" href="./styles/game.css" type="text/css">
 	<link rel="stylesheet" href="./styles/flip-card.css" type="text/css">
 	<script type="text/javascript" src="js/functions.js"></script>
 </head>
