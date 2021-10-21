0eb1b9440f1f69ef00a94e485e01435f606fd307
Carlos <darksunreaper@gmail.com>
2021-10-21 19:53:05 +0200

    Generated cards by difficulty

--- backendFunctions/functions.php
+++ backendFunctions/functions.php
@@ -8,23 +8,88 @@
         fwrite($hallOfFame, $results);
     }
 
-// taulells de 4x2, 4x3, 4x4, 5x4, 6x5, 8x5
+    // This function handles all the functions to generate the cards
 
     function generateCards($difficulty){
 
-        var_dump(difficultySelector($difficulty));
+        $gameTable = difficultySelector($difficulty);
 
-        // We make an array with all the cards and then we choose them randomly
+		$cardsArray = cardsSelection($gameTable["columns"], $gameTable["rows"]);
+      
+        printCards($cardsArray);
 
-		$cardsArray = ["blueEyesWhiteDragon","blueEyesWhiteDragon", 
-		"darkMagician","darkMagician", 
-		"potOfGreed","potOfGreed", 
-		"redEyesBlackDragon","redEyesBlackDragon"];					
-					
-		foreach ($cardsArray as $key) {
-			$random = rand(0,count($cardsArray)-1);
-		 	$nameCard = $cardsArray[$random];
+    }
 
+    // This function returns the columns and rows depending on difficulty
+
+    function difficultySelector($difficulty) {
+        if ($difficulty == "1") {
+            return ["columns" => 4, "rows" => 2];
+
+        } elseif ($difficulty == "2") {
+            return ["columns" => 4, "rows" => 3];
+
+        } elseif ($difficulty == "3") {
+            return ["columns" => 4, "rows" => 4];
+
+        } elseif ($difficulty == "4") {
+            return ["columns" => 5, "rows" => 4];
+
+        } elseif ($difficulty == "5") {
+            return ["columns" => 6, "rows" => 5];
+
+        } elseif ($difficulty == "6") {
+            return ["columns" => 8, "rows" => 5];
+        }
+    }
+
+    // This function returns an array of cards randomly selected
+
+    function cardsSelection($columns, $rows) {
+        $quantityOfCards = ($columns * $rows) / 2;
+        $gameCards = [];
+
+        $cardsArray = [["blueEyesWhiteDragon","blueEyesWhiteDragon"], 
+        ["darkMagician","darkMagician"], 
+        ["potOfGreed","potOfGreed"], 
+        ["redEyesBlackDragon","redEyesBlackDragon"],
+        ["BBusterDrake","BBusterDrake"],
+        ["ExodiatheForbiddenOne","ExodiatheForbiddenOne"],
+        ["LeftArmoftheForbiddenOne","LeftArmoftheForbiddenOne"],
+        ["LeftLegoftheForbiddenOne","LeftLegoftheForbiddenOne"],
+        ["RightArmoftheForbiddenOne","RightArmoftheForbiddenOne"],
+        ["RightLegoftheForbiddenOne","RightLegoftheForbiddenOne"],
+        ["MegalosmasherX","MegalosmasherX"],
+        ["ToonBusterBlader","ToonBusterBlader"],
+        ["Buten","Buten"],
+        ["MessengerofPeace","MessengerofPeace"],
+        ["ReturnoftheDragonLords","ReturnoftheDragonLords"],
+        ["ThornofMalice","ThornofMalice"],
+        ["FloodgateTrapHole","FloodgateTrapHole"],
+        ["MirrorForce","MirrorForce"],
+        ["CalloftheHaunted","CalloftheHaunted"],
+        ["GemKnightPearl","GemKnightPearl"],
+        ["FormulaSynchron","FormulaSynchron"]
+        ];
+
+        for ($i=0; $i < $quantityOfCards; $i++) { 
+            $randomCouple = rand(0,count($cardsArray)-1);
+            array_push($gameCards, $cardsArray[$randomCouple]);
+            array_splice($cardsArray, $randomCouple, 1);
+        }
+
+        return $gameCards;
+    }
+
+    // This function prints the cards into the webpage
+
+    function printCards($cardsArray) {
+
+        $cardsIntoPlay = count($cardsArray) * 2;
+
+        for ($i=0; $i < $cardsIntoPlay; $i++) { 
+            $random = rand(0, count($cardsArray)-1);
+		 	$nameCard = $cardsArray[$random][0];
 			echo "
 				<div class='card' name='$nameCard' state='unflipped' onclick='flip(this)'>	
 					<div class='card-inner'>
@@ -38,12 +103,12 @@
 				</div>						
 				";
 
-			array_splice($cardsArray,$random,1);
-		 }
-    }
+			array_splice($cardsArray[$random], 0,1);
 
-    function difficultySelector($difficulty) {
-        if ($difficulty = 1) return $table = ["rows" => 2, "columns" => 4];
+            if(count($cardsArray[$random]) == 0) {
+                array_splice($cardsArray, $random, 1);
+            }
+        }
     }
 
 ?>
