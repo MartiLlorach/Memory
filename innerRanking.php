a55de8bf5a4d56136f8cf0b9c3e769389a9930ea
martiLlo <martillorachnaharro@gmail.com>
2021-10-22 18:07:55 +0200

    background of each player in rank

--- innerRanking.php
+++ innerRanking.php
@@ -2,12 +2,12 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="./styles/general.css">
+	<link rel="stylesheet" type="text/css" href="./styles/innerRanking.css">
 </head>
 <body>
 	<div class="flex">
-		<div class="player" style="">
-			aaaaaaaaa
+		<div class="player">
+			<p>AAAAAAAaa</p>
 		</div>
 	</div>
 </body>
