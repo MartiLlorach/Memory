951142694fff422aabbf23ab3238ab8d20b6359f
martiLlo <martillorachnaharro@gmail.com>
2021-10-27 19:28:46 +0200

    Merge preproduction into main

---
+++ ranking.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="./styles/ranking.css">
+</head>
+<body>
+	<div class="mainDiv">
+		<div class="title">
+			<h2>Ranking</h2>
+		</div>
+		<div class="rankingContainer">
+			<?php include 'innerRanking.php'; ?>
+		</div>
+		<a href="index.php"><button class="btn">Main menu</button></a>
+	</div>	
+	
+</body>
+</html>
